8ef100a37a258c51648130af6ba86c5ede9c7349
Hassibur Rahman <md9178558@gmail.com>
2026-03-26 17:37:17 +0600

    added nav

--- src/components/AiTools.jsx
+++ src/components/AiTools.jsx
@@ -9,7 +9,7 @@ const AiTools = () => {
     const { user } = useUser();
 
     return (
-        <div className='px-4 sm:px-20 xl:px-32 my-24'>
+        <div className='px-4 sm:px-20 xl:px-32 my-18'>
 
             <div className='text-center'>
                 <h2 className='text-slate-700 text-[42px] font-semibold'>Powerful AI Tools</h2>
--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Menu, X } from 'lucide-react';
+import React, { use, useState } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { assets } from '../assets/assets';
 
 const Layout = () => {
-    return (
-        <div>
-            <h1>Layout</h1>
-            <Outlet />
 
+    const navigate = useNavigate();
+    const [sidebar, setSidebar] = useState(false);
+
+    return (
+        <div className='flex flex-col items-start justify-start h-screen'>
+
+            <nav className='w-full px-8 min-h-14 flex items-center justify-between border-b border-gray-200'>
+                <img src={assets.logo} alt="Logo" onClick={()=> navigate('/')} />
+
+                {
+                    sidebar ? <X onClick={()=> setSidebar(false)} className='w-6 h-6 text-gray-600 sm:hidden' />
+                    : <Menu onClick={()=> setSidebar(true)} className='w-6 h-6 text-gray-600 sm:hidden' />
+                }
+
+            </nav>
+            
+            <Outlet />
         </div>
     );
 };
