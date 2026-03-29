42233e206fdfb012535c4c92eea2bf121208f280
Hassibur Rahman <md9178558@gmail.com>
2026-03-29 22:33:09 +0600

    added write article

--- src/components/CreationItem.jsx
+++ src/components/CreationItem.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import  Markdown from 'react-markdown';  
 
 const CreationItem = ({item}) => {
 
@@ -25,8 +26,11 @@ const CreationItem = ({item}) => {
                             </div>
                         ) : (
                             <div className='mt-3 h-full overflow-y-scroll text-sm text-slate-700'>
-                                <div>
-                                    {item.content}
+                                <div className='reset-tw'>
+                                    <Markdown>
+                                        {item.content}
+                                    </Markdown>
+                                    
                                 </div>
                             </div>
                         )}
