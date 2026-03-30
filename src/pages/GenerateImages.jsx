93efd27f4563ec0cb39c503a9684e6ee8d0e1e97
Hassibur Rahman <md9178558@gmail.com>
2026-03-30 14:01:40 +0600

    added new page

--- src/pages/GenerateImages.jsx
+++ src/pages/GenerateImages.jsx
@@ -1,9 +1,80 @@
-import React from 'react';
+import { ImageIcon, Sparkles, Hash } from 'lucide-react';
+import React, { useState } from 'react';
 
 const GenerateImages = () => {
+
+    const imageStyle = [ 'Realistic', 'Ghibli style', 'Anime style', 'Cartoon style', 'Fantasy style', 'Realistic style', '3D style', 'Portrait style'];
+        
+    const [selectedStyle, setSelectedStyle] = useState('Realistic');
+    const [input, setInput] = useState('');
+    const [publish, setPublish] = useState(false);
+    
+    const onSubmitHandler = async (e) => {
+            e.preventDefault();
+        }
+
     return (
-        <div>
-            <h1>Generate Images</h1>
+        <div className='h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700'>
+            {/* left col */}
+
+            <form onSubmit={onSubmitHandler} className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200'>
+
+                <div className='flex items-center gap-3'>
+                    <Sparkles className='w-6 text-[#00AD25]'/>
+                    <h1 className='text-xl font-semibold'>AI Image Generator</h1>
+                </div>
+                <p className='mt-6 text-sm font-medium'>Describe Your Image</p>
+
+                <textarea onChange={(e)=>setInput(e.target.value)} value={input} rows={4}  className='outline-none border border-gray-300 rounded-md text-sm mt-2 px-3 p-2 w-full' placeholder='Describe the image you want to generate...' required  />
+
+                <p className='mt-4 text-sm font-medium'>Style</p>
+
+                <div className='mt-3 flex gap-3 flex-wrap sm:max-w-9/11'>
+                    {imageStyle.map((item)=>(
+                        <span onClick={()=> setSelectedStyle(item)}
+                         className={`text-xs px-4 py-1 border rounded-full cursor-pointer ${selectedStyle === item ? 'bg-green-50 text-green-700' : 'text-gray-500 border-gray-300'} `}key={item}>{item}</span>
+                    ) )}
+                </div>
+
+                    <div className='my-6 flex items-center gap-2'>
+                        <label className='relative cursor-pointer'>
+                            <input type="checkbox" onChange={(e)=> setPublish(e.target.checked)} checked={publish} className='sr-only peer' />
+                            <div className='w-9 h-5 bg-slate-300 rounded-full peer-checked:bg-green-500 transition'></div>
+                            <span className='absolute left-1 top-1 w-3 h-3 bg-white rounded-full transition peer-checked:translate-x-4 '></span>
+                        </label>
+                        <p className='text-sm'>Make this image public</p>
+                    </div>
+
+                
+                    
+                <button className='flex justify-center items-center gap-2 w-full bg-gradient-to-r from-[#00AD25] to-[#04FF50] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer'>
+                    <ImageIcon className='w-5' />
+                        Generate Image
+                </button>
+
+
+
+            </form>
+
+            {/* right col */}
+
+            <div className='w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200 flex flex-col min-h-96'>
+                <div className='flex items-center gap-3'>
+                    <ImageIcon className='w-5 h-5 text-[#00AD25]'/>
+                    <h1 className='text-xl font-semibold'>Generated Image</h1>
+                </div>
+
+                <div className='flex-1 flex justify-center items-center'>
+                    <div className='text-sm flex flex-col items-center gap-5 text-gray-400'>
+                        <Hash className='w-9 h-9 ' />
+                        <p>Enter a topic and click “Generate title” to get started</p>
+
+                    </div>
+                </div>
+
+                
+            </div>
+            
         </div>
     );
 };
